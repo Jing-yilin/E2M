5c84c73f4e1f1d5ca7f3de7c23109d892a579e45
Jing-yilin <yilin.jing.ai@outlook.com>
2024-06-01 23:06:25 +0800

    🦄 refactor: better structure

--- app/api/core/marker/models.py
+++ app/api/core/marker/models.py
@@ -1,3 +1,4 @@
+import logging
 from api.core.marker.postprocessors.editor import load_editing_model
 from surya.model.detection import segformer
 from texify.model.model import load_model as load_texify_model
@@ -9,45 +10,40 @@ from surya.model.recognition.processor import (
 )
 from surya.model.ordering.model import load_model as load_order_model
 from surya.model.ordering.processor import load_processor as load_order_processor
-import logging
 
 logger = logging.getLogger(__name__)
 
 
 class ModelHandler:
-    # Singleton class to load all models
+    _instance = None
 
     def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, "instance"):
-            cls.instance = super(ModelHandler, cls).__new__(cls)
-        return cls.instance
+        if cls._instance is None:
+            cls._instance = super(ModelHandler, cls).__new__(cls)
+        return cls._instance
 
     def __init__(self, langs=None):
-        self.models = self.load_all_models(langs)
+        if not hasattr(self, "models"):
+            self.models = self.load_all_models(langs)
 
     @classmethod
     def get_models(cls, langs=None):
-        if not hasattr(cls, "instance"):
-            cls.instance = cls(langs)
-        return cls.instance.models
+        return cls(langs).models
 
     def load_all_models(self, langs=None):
         logger.info("Loading all models")
-        # langs is optional list of languages to prune from recognition MoE model
         detection = setup_detection_model()
         layout = setup_layout_model()
         order = setup_order_model()
         edit = load_editing_model()
 
-        # Only load recognition model if we'll need it for all pdfs
         ocr = (
             setup_recognition_model(langs)
             if (settings.OCR_ENGINE == "surya" and settings.OCR_ALL_PAGES)
             else None
         )
         texify = setup_texify_model()
-        model_lst = [texify, layout, order, edit, detection, ocr]
-        return model_lst
+        return [texify, layout, order, edit, detection, ocr]
 
 
 def setup_recognition_model(langs):
@@ -79,6 +75,7 @@ def setup_texify_model():
 
 
 def setup_layout_model():
+    logger.info("Loading layout model")
     model = segformer.load_model(checkpoint=settings.LAYOUT_MODEL_CHECKPOINT)
     processor = segformer.load_processor(checkpoint=settings.LAYOUT_MODEL_CHECKPOINT)
     model.processor = processor
@@ -86,6 +83,7 @@ def setup_layout_model():
 
 
 def setup_order_model():
+    logger.info("Loading order model")
     model = load_order_model()
     processor = load_order_processor()
     model.processor = processor
