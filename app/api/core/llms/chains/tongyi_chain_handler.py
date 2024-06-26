c47b89fd5a934d1624172cba05d451724f5ac0b0
Jing-yilin <yilin.jing.ai@outlook.com>
2024-06-27 00:45:55 +0800

    🦄 refactor: optimized chain_handler structure

--- app/api/core/llms/chains/tongyi_chain_handler.py
+++ app/api/core/llms/chains/tongyi_chain_handler.py
@@ -1,16 +1,7 @@
 import logging
-from langchain_community.chat_models import ChatTongyi
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
-from langchain_core.runnables.base import RunnableSequence
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.language_models import BaseChatModel
 from api.core.llms.chains.base_chain_handler import BaseChainHandler
-from api.core.llms.prompts import (
-    OCR_FIX_TO_MARKDOWN_PROMPT,
-    OCR_FIX_TO_JSON_PROMPT,
-    EXTRACT_MARKDOWN_PROMPT,
-    EXTRACT_JSON_PROMPT,
-    COMMENT_PROMTPT,
-)
 from api.config import Config
 
 logger = logging.getLogger(__name__)
@@ -40,76 +31,5 @@ class TongyiChainHandler(BaseChainHandler):
 
         super().__init__(model)
 
-    def ocr_fix_to_markdown_chain(
-        self, model=None, comment: str = None
-    ) -> RunnableSequence:
-        if model is None:
-            model = self.model
-        hash_key = self._get_hash_key(
-            chain_name="ocr_fix_to_markdown", model=model, comment=comment
-        )
-        if hash_key not in self.chains:
-            chat_model = ChatTongyi(model=model)
-            messages = [("human", OCR_FIX_TO_MARKDOWN_PROMPT)]
-            if comment:
-                messages.append(("human", COMMENT_PROMTPT))
-            prompt_template = ChatPromptTemplate.from_messages(messages)
-            parser = StrOutputParser()
-            chain = prompt_template | chat_model | parser
-            self.chains[hash_key] = chain
-        return self.chains[hash_key]
-
-    def ocr_fix_to_json_chain(
-        self, model=None, comment: str = None
-    ) -> RunnableSequence:
-        if model is None:
-            model = self.model
-        hash_key = self._get_hash_key(
-            chain_name="ocr_fix_to_json", model=model, comment=comment
-        )
-        if hash_key not in self.chains:
-            chat_model = ChatTongyi(model=model)
-            messages = [("human", OCR_FIX_TO_JSON_PROMPT)]
-            if comment:
-                messages.append(("human", COMMENT_PROMTPT))
-            prompt_template = ChatPromptTemplate.from_messages(messages)
-            parser = JsonOutputParser()
-            chain = prompt_template | chat_model | parser
-            self.chains[hash_key] = chain
-        return self.chains[hash_key]
-
-    def extract_markdown_chain(
-        self, model=None, comment: str = None
-    ) -> RunnableSequence:
-        if model is None:
-            model = self.model
-        hash_key = self._get_hash_key(
-            chain_name="extract_markdown", model=model, comment=comment
-        )
-        if hash_key not in self.chains:
-            chat_model = ChatTongyi(model=model)
-            messages = [("human", EXTRACT_MARKDOWN_PROMPT)]
-            if comment:
-                messages.append(("human", COMMENT_PROMTPT))
-            prompt_template = ChatPromptTemplate.from_messages(messages)
-            parser = StrOutputParser()
-            chain = prompt_template | chat_model | parser
-            self.chains[hash_key] = chain
-        return self.chains[hash_key]
-
-    def extract_json_chain(self, model=None, comment: str = None) -> RunnableSequence:
-        if model is None:
-            model = self.model
-        hash_key = self._get_hash_key(
-            chain_name="extract_json", model=model, comment=comment
-        )
-        if hash_key not in self.chains:
-            chat_model = ChatTongyi(model=model)
-            messages = [("human", EXTRACT_JSON_PROMPT)]
-            if comment:
-                messages.append(("human", COMMENT_PROMTPT))
-            prompt_template = ChatPromptTemplate.from_messages(messages)
-            parser = JsonOutputParser()
-            chain = prompt_template | chat_model | parser
-            self.chains[hash_key] = chain
-        return self.chains[hash_key]
+    def init_chat_model(self, model) -> BaseChatModel:
+        return ChatTongyi(model=model, api_key=Config.DASHSCOPE_API_KEY)
